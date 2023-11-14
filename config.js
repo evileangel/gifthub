fb7f4f279d5d49229815a17f944db2c052b43917
evileangel <150454461+evileangel@users.noreply.github.com>
2023-11-14 17:05:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
     introTitle: 'Xin chào Nam nha!',
     introDesc: `Trong nắng vàng se hơi lạnh,
-    Tháng mười một đến thật nhanh
-    Có dáng người thêm một tuổi
+    Tháng mười một đến thật nhanh.
+    Có dáng người thêm một tuổi,
     Gửi lời chúc buổi hoàng hôn`,
     btnIntro: 'hihi',
     title: 'Mừng sinh nhật cậu nha, chúc Nam tuổi mới đẹp lại càng đẹp thêm, tuổi mới cũng thêm nguyện vọng mới và đặc biệt nhất là đều thành công nhé! 😙',
     desc: 'Ngày hôm nay của cậu như thế nào? ',
     btnYes: 'Rất vui luôn á <33',
     btnNo: 'Không, thật nhạt nhẽo :3',
-    question:'Dù một ngày có tồi tệ đến mấy, tớ mong cậu sẽ luôn tươi cười để vượt qua, nếu có thể thì hãy chia sẻ với tớ nha <3',
+    question:'Dù một ngày có tồi tệ đến mấy, tớ mong cậu sẽ luôn tươi cười để vượt qua, hãy chia sẻ với tớ nha <3, viết gì đi nè:',
     btnReply: 'Gửi cho Lý Hải',
     reply: 'Lý Hải có tí xíu đẹp trai mà dễ thương thì siêu cấp á <33333333',
     mess: 'Tớ biết mà 🥰. Tớ thích... 😘😘',
